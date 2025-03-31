7ab8ca3cfc395dd188226cdb62e2bf3e0b0246e5
OB4APKA <pila8922@gmail.com>
2025-03-31 18:47:39 +0500

    ура

---
+++ Assets/Scripts/Game/NPS_LaunchRocket.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class NPS_LaunchRocket : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject rocket;
+    private RocketLaunch button;
+    void Start()
+    {
+        button = rocket.GetComponent<RocketLaunch>();
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animator.SetTrigger("LaunchRocket");
+            button.StartPolet();
+        }
+    }
+}
