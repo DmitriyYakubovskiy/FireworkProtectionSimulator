d466a241f87477a00f8c86a1fd1bb199ba4647f5
OB4APKA <pila8922@gmail.com>
2025-04-15 23:13:05 +0500

    update DateSim BugFix 0.012

--- Assets/Main/Scripts/Game/NPS_LaunchRocket.cs
+++ Assets/Main/Scripts/Game/NPS_LaunchRocket.cs
@@ -3,19 +3,10 @@ using UnityEngine;
 public class NPS_LaunchRocket : MonoBehaviour
 {
     [SerializeField] private Animator animator;
-<<<<<<< HEAD:Assets/Scripts/Game/NPS_LaunchRocket.cs
-    [SerializeField] private GameObject rocket;
-    [SerializeField] private DialogueWindow dialogueWindow;
-    private RocketLaunch button;
-    void Start()
-    {
-        button = rocket.GetComponent<RocketLaunch>();
-        
-    }
-=======
     [SerializeField] private GameObject[] rockets;
+    [SerializeField] private DialogueWindow dialogueWindow;
 
->>>>>>> 83fad3cec273c029d00074afeb1b110c57f5ac59:Assets/Main/Scripts/Game/NPS_LaunchRocket.cs
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
